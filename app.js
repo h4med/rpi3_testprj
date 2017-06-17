7e171e5b6001638e14e81e5f2a57132ca0ae4e65
h4med <hamed.d@gmail.com>
2017-06-17 11:21:42 +0000

    views & public added

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ var express = require('express');
 
 var app = express();
 
+app.set('view engine','pug');
+
 app.get('/', function(req, res){
-	res.render('index.pug', {name: 'Hamed Davandeh'});
+	res.render('index', {
+		title: 'My App',
+		name: 'Hamed Davandeh'
+	});
 });
 
 
